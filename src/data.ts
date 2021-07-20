8dfaeb2d1be0fefc93f7a4169a3d2bd093cc86f9
Axel Lavoie <axel.lavoie@gmail.com>
2021-07-20 10:25:04 +0200

    💄 Lint

--- src/data.ts
+++ src/data.ts
@@ -2,7 +2,9 @@ import { readFile } from "fs/promises";
 import { Data, LoadData, convertData } from "./helpers.js";
 
 export const getData = async (symbol: string): Promise<Data[]> => {
-  const loadData: LoadData[] = JSON.parse(await readFile(`./data/1626626949735/${symbol}-1d.json`, "utf-8"));
+  const loadData: LoadData[] = JSON.parse(
+    await readFile(`./data/1626626949735/${symbol}-1d.json`, "utf-8")
+  );
   const data = loadData.map(convertData);
   return data;
-}
+};
